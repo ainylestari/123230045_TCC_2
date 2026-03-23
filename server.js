6885ce8f79527dfe560af38a2db689109e80091d
Aini Lestari <ainilestari206@gmail.com>
2026-03-23 16:01:01 +0000

    update dari VM

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const notesRoutes = require('./routes/notes');
 
@@ -7,10 +8,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/notes', notesRoutes);
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
     console.log('Server jalan di http://localhost:3000/notes');
 });
 
